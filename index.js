5a4edc536e13a6b53dce14dd5f1a81c906dcbe69
Arifur Rahman <arif.bd08060@gmail.com>
2023-03-24 20:39:13 +0600

    token insert

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express=require('express');
 const app=express();
+const jwt=require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId=require('mongodb').ObjectId;
 const cors=require('cors');
+const { query } = require('express');
 app.use(cors());
 app.use(express.json());
 require('dotenv').config();
@@ -15,11 +17,24 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 // const uri = "mongodb+srv://DoctorPortal:r1G2trY8CuCV9bDe@cluster0.pfe3z4d.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req,res,next){
+console.log("tocken verifyed",req.headers.authorization);
+const authHeader=req.headers.authorization;
+if(!authHeader){
+    return res.send(401).send("unautherised access");
+}
+const token=authHeader.split(' ')[1]; 
+}
+
+
+
 async function run(){
     try{
 await client.connect();
 const appoinmentOptionCollection=client.db("Doctors-Portal2").collection("appoinmentOption2");
 const bookingsCollection=client.db("Doctors-Portal2").collection("bookings");
+const usersCollection=client.db("Doctors-Portal2").collection("users");
+//user is test case so no need to accept it 
 const addUser=client.db("Doctors-Portal2").collection("user");
 //get data from db
 app.get('/appoinmentOption2',async(req,res)=>{
@@ -49,15 +64,40 @@ res.send(options);
 
 })
 //get bookings data from database
-app.get('/bookings',async(req,res)=>{
+app.get('/bookings',verifyJWT,async(req,res)=>{
     const email=req.query.email;
-    console.log(email);
+//    console.log(req.headers.authorization);
+    // console.log(email);
     const query={email:email}
     const booking=await bookingsCollection.find(query).toArray();
     res.send(booking);
 })
 
-//post data from user
+
+//post data from the user
+app.post('/users',async(req,res)=>{
+    const users=req.body;
+    const result =await usersCollection.insertOne(users);
+    res.send(result);
+})
+
+//get data from jwt
+//important note get data from database and show data inside get url.this url made by me what i want
+app.get('/jwt',async(req,res)=>{
+    const email=req.query.email;
+   const  query={email:email}
+    const user =await usersCollection.findOne(query);
+    if(user){
+        const token=jwt.sign({email},process.env.ACCESS_TOCKEN, { expiresIn:'1h' })
+        return  res.send({accessToken:token})
+    }
+    console.log(user);
+    res.status(404).send({accessToken:" token is unavailable"});
+
+
+})
+
+//post data from user this is test case
 app.post('/bookings',async(req,res)=>{
 
     const bookingss=req.body;
